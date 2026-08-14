b9c68a995332c0a3c6df509d73c01fcd2f78255a
SumitMahato410 <sumitmahato5448@gmail.com>
2026-08-14 15:09:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
